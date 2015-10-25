7119e8e9470b14cdec0698d4f89bc6492cf57ce5
Zareek Galván <zareek99@gmail.com>
2015-10-25 09:55:01 -0600

    Initial Commit

---
+++ FloydWarshall/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FloydWarshall
+//
+//  Created by Zareek Galvan on 10/25/15.
+//  Copyright © 2015 Trisquel Labs. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
